6952c1aaa7aa49ceb899e4d1ffef32cbadffdb92
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-06-22 22:02:54 -0700

    Revert "Merge branch 'main' into old"
    This reverts commit 7087547ea26bec37216e955e8566ed74fa10ea67.

--- build-ts/lib/SM/AnimRecorder.ts
+++ build-ts/lib/SM/AnimRecorder.ts
@@ -1,133 +1,5 @@
-class AnimationCurve {
-  constructor(precision = 1e-3) {
-    this.frames = new Float32Array(32);
-    this.length = 0;
-    this.precision = precision;
-  }
-  add(time, value) {
-    let n = this.length;
-    if (n * 2 == this.frames.length) {
-      const frames = new Float32Array(this.frames.length * 2);
-      frames.set(this.frames);
-      this.frames = frames;
-    }
-    while (n >= 2) {
-      const time0 = this.frames[n * 2 - 4],
-        value0 = this.frames[n * 2 - 3];
-      const time1 = this.frames[n * 2 - 2],
-        value1 = this.frames[n * 2 - 1];
-      const time2 = time,
-        value2 = value;
-      const area = Math.abs(
-        +time0 * value1 -
-          time1 * value0 +
-          time1 * value2 -
-          time2 * value1 +
-          time2 * value0 -
-          time0 * value2
-      );
-
-      if (area < this.precision) n--;
-      else break;
-    }
-    this.frames[n * 2 + 0] = time;
-    this.frames[n * 2 + 1] = value;
-    this.length = n + 1;
-  }
-}
-
-function serializeAnimationClip(animCurves, name) {
-  const output = [];
-
-  output.push(
-    `%YAML 1.1
-%TAG !u! tag:unity3d.com,2011:
---- !u!74 &7400000
-AnimationClip:
-  m_ObjectHideFlags: 0
-  m_CorrespondingSourceObject: {fileID: 0}
-  m_PrefabInstance: {fileID: 0}
-  m_PrefabAsset: {fileID: 0}
-  m_Name: ${name}
-  serializedVersion: 6
-  m_Legacy: 0
-  m_Compressed: 0
-  m_UseHighQualityCurve: 1
-  m_RotationCurves: []
-  m_CompressedRotationCurves: []
-  m_EulerCurves: []
-  m_PositionCurves: []
-  m_ScaleCurves: []
-  m_FloatCurves:
-`
-  );
-  let maxTime = 0;
-  for (let [curveName, curve] of animCurves) {
-    if (curve.length)
-      maxTime = Math.max(maxTime, curve.frames[curve.length * 2 - 2]);
-    output.push(
-      `  - curve:
-      serializedVersion: 2
-      m_Curve:
-`
-    );
-    for (let i = 0; i < curve.length; i++)
-      output.push(
-        `      - serializedVersion: 2
-        time: ${curve.frames[i * 2 + 0].toFixed(4)}
-        value: ${curve.frames[i * 2 + 1].toFixed(4)}
-`
-      );
-    output.push(
-      `      m_PreInfinity: 2
-      m_PostInfinity: 2
-      m_RotationOrder: 4
-    attribute: ${curveName}
-    path:
-    classID: 95
-    script:
-`
-    );
-  }
-  output.push(
-    `  m_PPtrCurves: []
-  m_SampleRate: 30
-  m_WrapMode: 0
-  m_Bounds:
-    m_Center: {x: 0, y: 0, z: 0}
-    m_Extent: {x: 0, y: 0, z: 0}
-  m_ClipBindingConstant:
-    genericBindings: []
-    pptrCurveMapping: []
-  m_AnimationClipSettings:
-    serializedVersion: 2
-    m_AdditiveReferencePoseClip: {fileID: 0}
-    m_AdditiveReferencePoseTime: 0
-    m_StartTime: 0
-    m_StopTime: ${maxTime.toFixed(4)}
-    m_OrientationOffsetY: 0
-    m_Level: 0
-    m_CycleOffset: 0
-    m_HasAdditiveReferencePose: 0
-    m_LoopTime: 0
-    m_LoopBlend: 0
-    m_LoopBlendOrientation: 1
-    m_LoopBlendPositionY: 1
-    m_LoopBlendPositionXZ: 1
-    m_KeepOriginalOrientation: 1
-    m_KeepOriginalPositionY: 1
-    m_KeepOriginalPositionXZ: 1
-    m_HeightFromFeet: 0
-    m_Mirror: 0
-  m_EditorCurves: []
-  m_EulerEditorCurves: []
-  m_HasGenericRootTransform: 0
-  m_HasMotionFloatCurves: 0
-  m_Events: []
-`
-  );
-  return output;
-}
+import { quat } from "../gl-matrix.js";
+import { AnimationCurve, SerializeAnimationClip } from "./AnimationCurve.js";
 
 const AnimatorMuscleName = [
   "Spine Front-Back",
@@ -243,6 +115,7 @@ export class AnimRecorder {
 
     this.isRecording = false;
     this.currentTime = 0;
+    this.lastRootQ = quat.create();
 
     this.curves = new Map(
       [...RootMotionNames, ...AnimatorMuscleName].map((name) => [
@@ -253,32 +126,47 @@ export class AnimRecorder {
     this.deltaTime = 0;
     this.bucket = 0;
   }
-  takeSnapshot(deltaTime) {
+  TakeSnapshot(deltaTime) {
     this.deltaTime += deltaTime;
     this.bucket += deltaTime * this.frameRate;
     if (this.bucket < 1) return;
     this.bucket %= 1;
 
     const pose = this.getHumanPose();
+    // EnsureQuaternionContinuity
+    if (quat.dot(pose.rootQ, this.lastRootQ) < 0)
+      quat.scale(pose.rootQ, pose.rootQ, -1);
+    this.lastRootQ.set(pose.rootQ);
 
     if (this.isRecording) this.currentTime += this.deltaTime;
     this.deltaTime = 0;
     this.isRecording = true;
 
     for (let i = 0; i < pose.rootT.length; i++)
-      this.curves.get(`RootT.${"xyz"[i]}`).add(this.currentTime, pose.rootT[i]);
+      this.curves.get(`RootT.${"xyz"[i]}`).Add(this.currentTime, pose.rootT[i]);
     for (let i = 0; i < pose.rootQ.length; i++)
       this.curves
         .get(`RootQ.${"xyzw"[i]}`)
-        .add(this.currentTime, pose.rootQ[i]);
+        .Add(this.currentTime, pose.rootQ[i]);
     for (let i = 0; i < pose.muscles.length; i++)
       this.curves
         .get(AnimatorMuscleName[i])
-        .add(this.currentTime, pose.muscles[i]);
+        .Add(this.currentTime, pose.muscles[i]);
   }
-  saveToClip(name = "clip") {
-    return new Blob(serializeAnimationClip(this.curves, name), {
-      type: "text/yaml",
-    });
+  SaveToClip(name = "clip") {
+    const curves = [...this.curves].map(([name, curve]) => [
+      "",
+      "Animator",
+      name,
+      curve,
+    ]);
+    return new Blob(
+      SerializeAnimationClip({
+        name: name,
+        curves: curves,
+        frameRate: this.frameRate,
+      }),
+      { type: "text/yaml" }
+    );
   }
 }
